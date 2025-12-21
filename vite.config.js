11c0e24bc545f9d0442035b9510648a42bd93237
beeve111 <39862732+beeve111@users.noreply.github.com>
2025-12-21 19:12:02 +0000

    Add vite config

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+installGlobals();
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    hmr: {
+      port: 3000,
+      protocol: "ws",
+    },
+  },
+  plugins: [
+    remix({
+      ignoredRouteFiles: ["**/.*"],
+    }),
+    tsconfigPaths(),
+  ],
+  build: {
+    target: "esnext",
+  },
+});
