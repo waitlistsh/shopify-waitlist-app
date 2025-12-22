0fe6de0b6d5fc56de131fa7ec13c5582a8937fe5
beeve111 <39862732+beeve111@users.noreply.github.com>
2025-12-22 18:40:18 +0000

    Reset to clean slate

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
-import { defineConfig } from "vite";
-
-installGlobals();
-
-export default defineConfig({
-  plugins: [remix()],
-});
